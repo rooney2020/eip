1d3fab63d6b193d4e051a783775720e3b7010ba3
Strong2019 <rooney.liyuan.zhao@kingland.com>
2020-10-07 09:06:23 +0000

    commit the little files of ioc project

---
+++ ioc/src/main/java/com/kingland/ioc/MocIoc.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 Kingland Systems Corporation. All Rights Reserved.
+ */
+package com.kingland.ioc;
+
+/**
+ * @author KSC
+ * @description
+ */
+public class MocIoc {
+}
---
+++ ioc/src/test/java/com/kingland/ioc/MocIocTest.java
@@ -0,0 +1,19 @@
+package com.kingland.ioc;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MocIocTest {
+
+    @BeforeAll
+    void setUp() {
+
+    }
+
+    @Test
+    public void testIoc() {
+
+    }
+}
