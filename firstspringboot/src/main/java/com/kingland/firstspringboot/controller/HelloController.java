f3abfe138c1b2fed3b336eefe1e492b5eeb4277d
Strong2019 <rooney.liyuan.zhao@kingland.com>
2020-10-13 03:26:51 +0000

    commit springboot practical project

--- firstspringboot/src/main/java/com/kingland/firstspringboot/controller/HelloController.java
+++ firstspringboot/src/main/java/com/kingland/firstspringboot/controller/HelloController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
-
 /**
  * @author KSC
  */
