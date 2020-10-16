fc3d0afab3d75736a17d202fc90f7134629c6a66
Strong2019 <rooney.liyuan.zhao@kingland.com>
2020-10-16 06:39:16 +0000

    commit spring day2 project

--- firstspringboot/src/main/java/com/kingland/firstspringboot/service/StudentServiceImpl.java
+++ firstspringboot/src/main/java/com/kingland/firstspringboot/service/StudentServiceImpl.java
@@ -5,6 +5,7 @@ package com.kingland.firstspringboot.service;
 
 import com.kingland.firstspringboot.mapper.StudentModelMapper;
 import com.kingland.firstspringboot.model.StudentModel;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,26 +22,31 @@ public class StudentServiceImpl implements StudentService {
         this.mapper = mapper;
     }
 
+    @Cacheable(cacheNames = "student")
     @Override
     public List<StudentModel> getAllStudents() {
         return mapper.getAllStudents();
     }
 
+    @Cacheable(cacheNames = "student", key = "#{id}")
     @Override
     public StudentModel getStudentById(long id) {
         return mapper.getStudentById(id);
     }
 
+    @Cacheable(cacheNames = "student", key = "#{id}")
     @Override
     public int addStudent(StudentModel student) {
         return mapper.addStudent(student);
     }
 
+    @Cacheable(cacheNames = "student", key = "#{id}")
     @Override
     public int updateStudentById(StudentModel student) {
         return mapper.updateStudentById(student);
     }
 
+    @Cacheable(cacheNames = "student", key = "#{id}")
     @Override
     public int deleteStudentById(long id) {
         return deleteStudentById(id);
